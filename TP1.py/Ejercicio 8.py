dc00544609f741b6e09e69e901138002de921263
Kevin-Oyarse <kevinoyarse6@gmail.com>
2022-04-07 19:54:30 -0300

    tp1

--- TP1.py/Ejercicio 8.py
+++ TP1.py/Ejercicio 8.py
@@ -2,17 +2,23 @@
 signos=["!","“","#","$","%","&",".","-"]
 vocales=["a","e","i","o","u"]
 cant=0
-
+f=0
 nombre=input('Ingresa su nombre y apellido:\n')
 nombre.lower()
-for i in vocales:
-    for j in nombre:
-        if i==j:
-            cant+=1
-if cant>3:
-    print ("El nombre que ingresaste tiene",cant,"vocales.")
-elif cant<3:
-    print ("El nombre tiene menos de 3 vocales.")
+while f==0:
+    f=0
+    for i in vocales:
+        for j in nombre:
+            if i==j:
+                cant+=1
+    if cant>3:
+        f=1
+        print ("El nombre que ingresaste tiene",cant,"vocales.")
+    elif cant<3:
+        f=0
+        print ("El nombre incorrecto.")
+        nombre=input('Ingresa su nombre y apellido:\n')
+        nombre.lower()
 
 
 año=input("ingresar año:\n")
@@ -26,16 +32,21 @@ while len(año)==len(año):
         print("ingrese año real")
         año=input("ingresar año:\n")
 
-numero=input("ingresar su contraseña:\n")
 contra=0
-for i in signos:
-    for j in numero:
-        if i==j:
-            contra+=1
-if contra<8 or contra>16:
-    print("tu contraseña debe tener entre 8 a 16 caracteres y tener algun caracter especial")
-elif contra>=8 and contra<=16:
-    print("tu contraseña tiene esta cantidad de caracteres",contra)
+fContra=0
+numero=input("ingresar su contraseña:\n")
+while fContra==0:
+    for i in signos:
+        for j in numero:
+            if i==j:
+                contra+=1
+    if contra<8 or contra>16:
+        print("tu contraseña debe tener entre 8 a 16 caracteres y tener algun caracter especial")
+        numero=input("ingresar su contraseña:\n")
+        fContra=0
+    elif contra>=8 and contra<=16:
+        print("tu contraseña tiene esta cantidad de caracteres",contra)
+        fContra=1
 
 
 
